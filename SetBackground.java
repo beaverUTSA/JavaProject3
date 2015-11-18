ff985343bdc8c7a6402242484cd3b59a858b22fd
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:02:46 -0600

    Create SetBackground.java

---
+++ SetBackground.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+public class SetBackground {
+  public static void main(String[] args) {
+    DrawingPanel panel = new DrawingPanel(300, 200);
+    panel.setBackground(Color.green);
+  }
+}
