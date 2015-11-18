5e8e0f0520b81ba98b021307dfa59c06aa83f0c7
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:08:47 -0600

    Create OldMacDonaldPara.java

---
+++ OldMacDonaldPara.java
@@ -0,0 +1,73 @@
+public class OldMacDonaldPara {
+  public static void title(){
+    System.out.println("Old MacDonald Had a farm");
+    System.out.println();
+  }
+  
+  public static void verse(){
+    char animal;
+    char noise;
+    
+    animal = cow;
+    noise = moo;
+    
+    System.out.println("And on his farm he had a " + animal + ", E-I-E-I-O");
+    System.out.println("With a " + noise + "-" + noise + " here and a " + noise + "-" + noise + " there");
+    System.out.println("Here a " + noise + " there a " + noise + "");
+    System.out.println("Everywhere a " + noise + "-" + noise + "");
+  }
+  
+  public static void oldMacLine(){
+    System.out.println("Old Macdonald had a farm, E-I-E-I-O");
+  }
+  
+  public static void firstVerse(){
+    
+
+    oldMacLine();
+    verse();
+    oldMacLine();
+    System.out.println();
+  }
+  
+  public static void secondVerse(){
+    oldMacLine();
+    System.out.println("And on his farm he had a pig, E-I-E-I-O");
+    System.out.println("With a oink-oink here and a oink-oink there");
+    System.out.println("Here a oink there a oink");
+    System.out.println("Everywhere a oink-oink");
+    System.out.println("With a moo-moo here and a moo-moo there");
+    System.out.println("Here a moo there a moo");
+    System.out.println("Everywhere a moo-moo");
+    oldMacLine();
+    System.out.println();
+  }
+  
+  public static void thirdVerse(){
+    oldMacLine();
+    System.out.println("And on his farm he had a horse, E-I-E-I-O");
+    System.out.println("With a neigh-neigh here and a neigh-neigh there");
+    System.out.println("Here a neigh there a neigh");
+    System.out.println("Everywhere a neigh-neigh");
+    System.out.println("With a oink-oink here and a oink-oink there");
+    System.out.println("Here a oink there a oink");
+    System.out.println("Everywhere a oink-oink");
+    System.out.println("With a moo-moo here and a moo-moo there");
+    System.out.println("Here a moo there a moo");
+    System.out.println("Everywhere a moo-moo");
+    oldMacLine();
+    System.out.println();
+  }
+  
+  public static void main(String[] args){
+    
+    title();
+    
+    firstVerse();
+    
+    secondVerse();
+    
+    thirdVerse();
+    
+  }
+}
