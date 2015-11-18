ce395425119a1b1a94f6126607c5f07959ce85d4
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:00:56 -0600

    Create TheWorld.java

---
+++ TheWorld.java
@@ -0,0 +1,6 @@
+public class TheWorld {
+  public static void main(String[] args){
+    System.out.println("The circumference of the world is "+ 40000 + " kilometers.");
+    System.out.println("This makes the diameter " + 40000/3.14 + " kilometers.");
+  }
+}
