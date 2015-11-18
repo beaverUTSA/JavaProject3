1cf23fddb0a99c45ee2a5ef8732001b2c77af4e1
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:14:22 -0600

    Create DoStuff.java

---
+++ DoStuff.java
@@ -0,0 +1,7 @@
+public class DoStuff{
+  public static void main(String[] args){
+    System.out.println("Hello\n");    
+    System.out.print("My name is Dave");    
+    System.out.println("What is your name?");
+  }
+}
