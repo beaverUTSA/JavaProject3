6f939543f425562bb0871f471c60dec688758122
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:00:17 -0600

    Create VolumeSphere.java

---
+++ VolumeSphere.java
@@ -0,0 +1,10 @@
+public class VolumeSphere {
+   public static void main (String[] args){
+      double radius;
+      double volume;
+      radius = 5;
+      volume = (4.0/3.0) * Math.PI * Math.pow(radius, 3);
+
+      System.out.println("The volume of a sphere with a radius of 5 is " + volume);
+   }
+}
