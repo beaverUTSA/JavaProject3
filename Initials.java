1ddd066c81571aa1530f292d099f562c04775eb4
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 08:56:35 -0600

    Create Initials.java

---
+++ Initials.java
@@ -0,0 +1,13 @@
+public class Initials {
+  public static void main(String[] args){
+    System.out.println("Lab 0 written by Brian Biever"); //// 
+    System.out.println(" ");                               //
+    System.out.println("BBBBBB  JJJJJJJ BBBBBB ");         // 
+    System.out.println("B     B    J    B     B");         //
+    System.out.println("B     B    J    B     B");         ////This prints out
+    System.out.println("BBBBBB     J    BBBBBB ");         //  pretty stuff
+    System.out.println("B     B J  J    B     B");         //
+    System.out.println("B     B J  J    B     B");         //
+    System.out.println("BBBBBB   JJ     BBBBBB ");       ////
+  }
+}
