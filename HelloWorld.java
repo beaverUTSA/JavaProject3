9517871c47aff178d30f9a3d79f8efbbad67f611
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:10:46 -0600

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println();
+    System.out.println("Take me to your leader!");
+    System.out.println("\"I'm sorry Dave.\n I'm afraid I can't do that.\"");
+    System.out.println("\t\t\t\\Hal/");
+  }
+} 
