368fc10653ded6eb5bc2eb60bf273e5540f257a7
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:05:59 -0600

    Create PrintSquares1a.java

---
+++ PrintSquares1a.java
@@ -0,0 +1,20 @@
+public class PrintSquares1a {
+  public static void main (String[] args) {
+    int count;
+    
+    count = 1;
+    
+    System.out.println(count + " squared is " + (count * count));
+    count++;
+    System.out.println(count + " squared is " + (count * count));
+    count++;
+    System.out.println(count + " squared is " + (count * count));
+    count++;
+    System.out.println(count + " squared is " + (count * count));
+    count++;
+    System.out.println(count + " squared is " + (count * count));
+    count++;
+    System.out.println(count + " squared is " + (count * count));
+    count++;
+  }
+}
