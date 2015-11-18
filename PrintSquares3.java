c2b6c6520e7e4b210e2c49d28564e4afdfb53062
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:05:05 -0600

    Create PrintSquares3.java

---
+++ PrintSquares3.java
@@ -0,0 +1,10 @@
+public class PrintSquares3 {
+  public static void main(String[] args) {
+    int count;
+    
+    for (count = 1; count <= 6; count++) {
+      System.out.print(count * count + " ");
+    }
+    System.out.println();
+  }
+}
