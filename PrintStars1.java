014efe222a6672009f3d06367874a82b1411c3e7
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:04:38 -0600

    Create PrintStars1.java

---
+++ PrintStars1.java
@@ -0,0 +1,28 @@
+public class PrintStars1 {
+  public static void main(String[] args) {
+    for (int column = 1; column <= 1; column++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    for (int column = 1; column <= 2; column++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    for (int column = 1; column <= 3; column++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    for (int column = 1; column <= 4; column++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    for (int column = 1; column <= 5; column++) {
+      System.out.print("*");
+    }
+    System.out.println();
+    for (int column = 1; column <= 6; column++) {
+      System.out.print("*");
+    }
+    System.out.println();
+  }
+}
