0a4ddc5c22fdd63d08b3d0b3f4e0c160c468586a
beaverdabeaver <sukuraz12@gmail.com>
2015-11-18 09:10:03 -0600

    Create LoopFun.java

---
+++ LoopFun.java
@@ -0,0 +1,10 @@
+public class LoopFun {
+  public static void main(String[] args) {
+    int i;
+    
+    for (i = 1; i <= 6; i+=2) {
+      System.out.print(i + " ");
+    }
+    System.out.println();
+  }
+}
